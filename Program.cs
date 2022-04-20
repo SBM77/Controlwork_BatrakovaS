c16883209701e07dff9ac037437ad44652e6b735
Sabina <mms8818@mail.ru>
2022-04-20 10:43:17 +0300

    add solving control task

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+var simvols = new string[]
+{
+    "hello",
+    "2",
+    "world",
+    ":-)"
+};
+ 
+for (int i = 0; i < simvols.Length; i++)
+{
+    if (simvols[i].Length <=3)
+        Console.WriteLine(simvols[i]);
+}
+
+
+
+
+
